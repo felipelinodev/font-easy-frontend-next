113e88f995523c4307d2890c2cc63c2130c334cd
felipelinodev <felipelinodesigndoe@gmail.com>
2026-01-07 21:59:50 -0300

    fix: remove from test

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,59 +1,9 @@
-// import { redirect } from "next/navigation";
-// import { cookies } from "next/headers";
-// import { ProfileContextProvider } from "../context/ProfileContext";
+// Layout temporário para testes - sem validações de autenticação
 
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "@/lib/auth";
-
-// export default async function RootLayout({
-//     children,
-// }: {
-//     children: React.ReactNode;
-// }) {
-
-//     const API_URL = process.env.BACKEND_URL!;
-
-//     // pega a sessão do googel meu fi
-//     const session = await getServerSession(authOptions);
-//     const cookiesList = await cookies();
-//     const authCookie = cookiesList.has("font-easy-auth");
-
-//     if (!authCookie && !session) { return redirect("/login"); }
-
-//     let userData;
-
-//     if (session) {
-//         userData = session.user
-//     } else {
-//         const res = await fetch(`${API_URL}/profile`, {
-//             headers: {
-//                 cookie: cookiesList.toString(),
-//             },
-//             cache: 'no-store',
-//             credentials: 'include',
-//         });
-
-//         if (!res.ok) { return redirect("/login"); }
-
-//         const text = await res.text();
-
-//         let user;
-
-//         try {
-//             user = JSON.parse(text);
-//         } catch (e) {
-//             return redirect("/login");
-//         }
-
-//         userData = user?.user || user;
-//     }
-
-
-
-
-//     return (
-//         <ProfileContextProvider user={userData}>
-//             {children}
-//         </ProfileContextProvider>
-//     )
-// }
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <>{children}</>;
+}
