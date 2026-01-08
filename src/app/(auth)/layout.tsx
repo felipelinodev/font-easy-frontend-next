c4e41c366bbc59204e6805b5ba0945f0ffd02ac7
felipelinodev <felipelinodesigndoe@gmail.com>
2026-01-07 22:05:41 -0300

    fix: from test

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,53 @@
-// Layout temporário para testes - sem validações de autenticação
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { ProfileContextProvider } from "../context/ProfileContext";
 
-export default function RootLayout({
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return <>{children}</>;
+
+    const API_URL = process.env.BACKEND_URL!;
+
+    // pega a sessão do googel meu fi
+    const session = await getServerSession(authOptions);
+    const cookiesList = await cookies();
+    const authCookie = cookiesList.has("font-easy-auth");
+
+    // if (!authCookie && !session) { return redirect("/login"); }
+
+    let userData;
+
+
+    const res = await fetch(`${API_URL}/profile`, {
+        headers: {
+            cookie: cookiesList.toString(),
+        },
+        cache: 'no-store',
+        credentials: 'include',
+    });
+
+
+    const text = await res.text();
+
+    let user;
+
+    try {
+        user = JSON.parse(text);
+    } catch (e) {
+
+    }
+
+    userData = user?.user || user;
+
+
+    return (
+        <ProfileContextProvider user={userData}>
+            {children}
+        </ProfileContextProvider>
+    )
 }
